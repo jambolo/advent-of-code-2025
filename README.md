4c2abd2cb3f214e64468863b8348c4db24494dc3
John Bolton <jambolo@users.noreply.github.com>
2025-12-03 17:33:55 -1000

    Day 01, part 2

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # advent-of-code-2025
 
 My solutions to Advent Of Code 2025 implemented in Rust. The development environment is VS Code and Windows.
+
+## Day 1
+
+Simple modulo arithmetic. Other than getting those annoying boundary conditions right, it was not much of a challenge.
--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,5 +1,4 @@
-// Day 1
-//
+// Advent of Code, 2025, Day 1
 
 use common::load;
 
@@ -7,31 +6,41 @@ const STARTING_POSITION: i32 = 50;
 const P: i32 = 100;
 
 fn main() {
-    println!("Day 1, part {}", if cfg!(feature="part2") { "2" } else { "1" });
+    println!("Day 1, part {}", if cfg!(feature = "part2") { "2" } else { "1" });
 
     // Load the data
     let lines = load::lines();
-    println!("Loaded {} lines of data.", lines.len());
 
     let mut current_position = STARTING_POSITION;
     let mut password = 0;
     for line in lines {
         // Parse the direction and distance. Format is e.g. "R2", "L3".
         let (turn, distance_str) = line.split_at(1);
-        let distance: i32 = distance_str.parse::<i32>().expect("Invalid distance") % P;
+        let distance = distance_str.parse::<i32>().expect("Invalid distance");
+        let full_turns = distance / P;
+        let remainder = distance % P;
 
-        match turn {
-            "R" => current_position = (current_position + distance) % P,
-            "L" => current_position = (current_position + P - distance) % P,
-            _ => panic!("Invalid turn direction: {}", turn),
+        // In part 2, count the number of times we pass position 0 (but not land on it)
+        if cfg!(feature = "part2") {
+            password += full_turns;
+            match turn {
+                "R" if current_position > P - remainder => password += 1,
+                "L" if (0 < current_position) && (current_position < remainder) => password += 1,
+                "R" | "L" => {},
+                _ => panic!("Invalid turn direction: {}", turn),
+            }
         }
+
+        current_position = match turn {
+            "R" => (current_position + remainder) % P,
+            "L" => (current_position + P - remainder) % P,
+            _ => panic!("Invalid turn direction: {}", turn),
+        };
 
         // Count the number of times the position is 0
         if current_position == 0 {
             password += 1;
         }
-
-        // Otherwise, part 2
     }
     println!("The password is: {}", password);
 }
